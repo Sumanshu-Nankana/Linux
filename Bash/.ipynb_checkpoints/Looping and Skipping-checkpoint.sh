e77383e5bdbf8b9ae392b524af3a97781487452f
Sumanshu Nankana <sumanshunankana@gmail.com>
2021-05-15 21:37:18 +0100

    Bash Script

---
+++ Bash/.ipynb_checkpoints/Looping and Skipping-checkpoint.sh
@@ -0,0 +1,6 @@
+# https://www.hackerrank.com/challenges/bash-tutorials---looping-and-skipping/problem
+# Run Loop from 1 to 99, with a step of 2
+for n in {1..99..2}
+    do
+      echo ${n}
+    done
---
+++ Bash/A Personalized Echo.sh
@@ -0,0 +1,7 @@
+# https://www.hackerrank.com/challenges/bash-tutorials---a-personalized-echo/problem
+
+# read the input from the user
+read name
+
+# display the output
+echo "Welcome $name"
