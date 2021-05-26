f42a20229127b90050112190c793642757ad0a69
Sumanshu Nankana <sumanshunankana@gmail.com>
2021-05-26 02:53:39 +0100

    Head

---
+++ Bash/Head of a Text File #1.sh
@@ -0,0 +1,9 @@
+# https://www.hackerrank.com/challenges/text-processing-head-1/problem
+
+# head [filename]
+# head -n 11 [filename]  -> First 11 lines  
+# head -c 20 [filename]  -> First 20 characters
+# tal -f [filename] => default 10 lines, -f is used so that it did not exit from terminal, and if file gets updated in real time it shows latest 10 lines
+
+# Method-1
+head -n 20
